88644cac7b7c7b12361121213919682e69ab573a
Rashid Saleem <mrashidcit@gmail.com>
2017-08-26 12:02:48 +0500

    question table added

---
+++ database/migrations/2017_08_26_063757_create_questions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('subject_id')->unsigned();
+            $table->string('a', 255);
+            $table->string('b', 255);
+            $table->string('c', 255);
+            $table->string('d', 255);
+            $table->string('correct_option', 3);
+            
+            $table->timestamps();
+
+            $table->foreign('subject_id')-> references('id')->on('subjects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
---
+++ database/seeds/SubjectsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SubjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        
+
+        DB:table('subjects')->insert([
+            name => 'Urdu'
+
+        ]);
+
+
+    }
+}
